323c28d2be6719f714cf9090b700efd95d2aea5a
Diana Liao <dijali@gmail.com>
2021-02-12 23:59:37 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -8,20 +8,19 @@ def apply_coupons(cart, coupons)
 
   cart.map do |thing|
     coupons.each do |sale|
-      if thing[:item] == sale[:item] && sale[:num] < thing[:count]
+      if thing[:item] == sale[:item] && sale[:num] <= thing[:count]
         sale_things = {
           :item => thing[:item].concat(" W/COUPON"),
           :price => sale[:cost]/sale[:num],
           :clearance => true,
           :count => sale[:num]
         }
-        binding.pry
         thing[:count] -= sale[:num]
         cart << sale_things
         
-      elsif thing[:item] == sale[:item] && sale[:num] >= thing[:count]
-        thing[:item] = thing[:item].concat(" W/COUPON")
-        thing[:price] = sale[:cost]/sale[:num]
+      #elsif thing[:item] == sale[:item] && sale[:num] >= thing[:count]
+        #thing[:item] = thing[:item].concat(" W/COUPON")
+        #thing[:price] = sale[:cost]/sale[:num]
       
       end 
       
