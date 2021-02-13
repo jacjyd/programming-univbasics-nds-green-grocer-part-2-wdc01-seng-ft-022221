6d2489055252a4c77a42b000ff39ecc7143406aa
Diana Liao <dijali@gmail.com>
2021-02-13 00:28:19 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -7,7 +7,7 @@ def apply_coupons(cart, coupons)
   # REMEMBER: This method **should** update cart
 
   cart.map do |thing|
-  
+  while thing[:count] > 0 do
     coupons.each do |sale|
       if thing[:item] == sale[:item] && sale[:num] <= thing[:count]
         sale_things = {
@@ -32,6 +32,7 @@ def apply_coupons(cart, coupons)
       end 
       
     end 
+  end
   end
 
   cart
